dc2968bb05545ddd163b4cc4bd6445b91a7afca0
liqilong <liqilong199126@gmail.com>
2020-05-25 12:45:53 +0800

    新增 Pug Ejs 模板引擎测试

--- server/index.js
+++ server/index.js
@@ -1,38 +1,15 @@
 const Koa = require('koa')
 const app = new Koa()
-const { normal } = require('./tpl')
+const { htmlTpl, ejsTpl, pugTpl } = require('./tpl')
+// const ejs = require('ejs')
+const pug = require('pug')
 
 app.use(async (ctx, next) => {
     ctx.type = 'text/html; charset=utf-8'
-    ctx.body = normal
+    ctx.body = pug.render(pugTpl, {
+        you: 'Luke',
+        me: 'Scott'
+    })
 })
 
 app.listen(4455)
-
-
-
-// const Koa = require('koa')
-// const logger = require('koa-logger')
-// const session = require('koa-session')
-// const app = new Koa()
-
-// app.keys = ['Hi Luke']
-
-// app.use(logger())
-// app.use(session(app))
-
-// app.use(ctx => {
-//     if (ctx.path === '/favicon.ico') return
-
-//     if (ctx.path === '/') {
-//        let n = ctx.session.views
-//        ctx.session.views = ++n
-//        ctx.body = n + ' 次'
-//     } else if (ctx.path == '/hi') {
-//         ctx.body = 'Hi Luke'
-//     } else {
-//         ctx.body = '404'
-//     }
-// })
-
-// app.listen(2333)
---
+++ server/tpl/ejs.js
@@ -0,0 +1,28 @@
+module.exports = `
+  <!DOCTYPE html>
+  <html>
+     <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="with=device-width, initial-scale=1">
+        <title>Koa Server HTML</title>
+        <link href="https://cdn.bootcss.com/bootstrap/4.0.0-beta.2/css/bootstrap.min.css" ref="stylesheet">
+        <srcipt src="https://cdn.bootcss.com/jquery/3.2.0/jquery.min.js"></script>
+        <srcipt src="https://cdn.bootcss.com/bootstrap/4.0.0-beta.2/js/bootstrap.bundle.min.js"></script>
+
+     </head>
+     <body>
+        <div class="container">
+           <div class="row">
+             <div class="col-md-8">
+                <h1>Hi <%= you %></h1>
+                <p>This is <%= me %></p>
+             </div>
+             <div class="col-md-4">
+                <p>测试动态 ejs 模板引擎</p>
+             </div>
+           </div>
+        </div>
+     </body>
+  </html>
+`
+
