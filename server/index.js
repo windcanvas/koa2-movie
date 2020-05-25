65c1128d5dc639ad77a62e200f52dfa0a648db5c
liqilong <liqilong199126@gmail.com>
2020-05-25 14:06:18 +0800

    增加模板引擎中间件

--- server/index.js
+++ server/index.js
@@ -1,12 +1,15 @@
 const Koa = require('koa')
+const views = require('koa-views')
+const { resolve } = require('path')
+
 const app = new Koa()
-const { htmlTpl, ejsTpl, pugTpl } = require('./tpl')
-// const ejs = require('ejs')
-const pug = require('pug')
+
+app.use(views(resolve(__dirname, './views'), {
+    extension: 'pug'
+}))
 
 app.use(async (ctx, next) => {
-    ctx.type = 'text/html; charset=utf-8'
-    ctx.body = pug.render(pugTpl, {
+    await ctx.render('index', {
         you: 'Luke',
         me: 'Scott'
     })
